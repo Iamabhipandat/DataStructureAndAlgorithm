79dc58a6ab5ab3be592d48142b9401f8c77d5dbf
Abhinav Tiwari <133164429+Iamabhipandat@users.noreply.github.com>
2026-07-28 20:32:20 +0530

    Time: 10 ms (27.43%), Space: 47.4 MB (6.86%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-                Arrays.sort(nums);
-            int low = 0;
+        Arrays.sort(nums);
+  int low = 0;
         int high = nums.length-1;
         int n = nums.length;
         if(n==1)return nums[0];
