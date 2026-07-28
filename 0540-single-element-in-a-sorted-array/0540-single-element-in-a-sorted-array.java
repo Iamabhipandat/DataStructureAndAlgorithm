57a32303b0a7648f56075f7f0936a3f84b6f8b4f
Abhinav Tiwari <133164429+Iamabhipandat@users.noreply.github.com>
2026-07-28 09:31:13 +0530

    Time: 0 ms (100%), Space: 53.1 MB (9.02%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
 
-        Arrays.sort(nums);
+       
         int low = 0;
         int high = nums.length-1;
         int n = nums.length;
